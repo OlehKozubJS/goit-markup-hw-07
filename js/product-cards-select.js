bb4916819795a908a1d1f019703f541a6fd8b2fa
OlehKozubJS <oleg.kozub54@gmail.com>
2023-03-05 19:28:28 +0200

    Update product-cards-select.js

--- js/product-cards-select.js
+++ js/product-cards-select.js
@@ -4,33 +4,22 @@ let productText = document.getElementsByClassName("product-description");
 
 let productCards = document.getElementsByClassName("product-card");
 
-let productCardClasses =
-{
-"allFilter":       [true, true, true, true, true, true, true, true, true],
-
-"appFilter":       [true, false, true, false, false, false, false, false, false],
-
-"marketingFilter": [false, true, false, true, false, true, false, false, false],
-
-"designFilter":    [false, false, false, false, true, false, true, false, false],
-
-"websiteFilter":   [false, false, false, false, false, false, false, true, true]
-}
+let productCardClasses = ["appFilter", "marketingFilter", "appFilter", "marketingFilter", "designFilter", "marketingFilter", "designFilter", "websiteFilter", "websiteFilter"];
 
 let productCardWidth = [100, 100, 100, 100, 100, 100, 100, 100, 100];
 
-let cardData = productCardClasses["allFilter"];
+let cardData = "allFilter";
 
 setInterval(cardAppearsAndDisappears, 1);
 
 function cardFilter(cardType) {
-    cardData = productCardClasses[cardType];
+    cardData = cardType;
 }
 
 function cardAppearsAndDisappears() {
     for (let itemIndex = 0; itemIndex < productCards.length; ++itemIndex) {
-        if (cardData[itemIndex] == true  && productCardWidth[itemIndex] < 100) ++productCardWidth[itemIndex];
-        if (cardData[itemIndex] == false && productCardWidth[itemIndex] >   0) --productCardWidth[itemIndex];
+        if ((productCardClasses[itemIndex] == cardData || cardData == "allFilter") && productCardWidth[itemIndex] < 100) ++productCardWidth[itemIndex];
+        if (productCardClasses[itemIndex] != cardData && cardData != "allFilter" && productCardWidth[itemIndex] > 0) --productCardWidth[itemIndex];
 
         productLinks[itemIndex].style = `width: ${productCardWidth[itemIndex]}%; overflow: hidden;`;
         productImages[itemIndex].style = `width: ${productCardWidth[itemIndex]}%; overflow: hidden;`;        
